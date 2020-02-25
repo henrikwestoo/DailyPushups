90a1ca71a81acc4204f2ea34a066a1bbbc667c90
Henrik Westöö <henrikwestoo@hotmail.com>
2020-02-25 15:45:58 +0100

    Fixed unique date entries to database, added basic timer on exercise activity

--- app/src/main/java/com/example/dailypushups/ExerciseActivity.java
+++ app/src/main/java/com/example/dailypushups/ExerciseActivity.java
@@ -7,9 +7,11 @@ import androidx.room.Room;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -18,6 +20,8 @@ import java.util.List;
 public class ExerciseActivity extends AppCompatActivity {
 
     String todaysDate;
+    EditText numberOfPushupsET;
+    TextView timerTxt;
 
 
     @Override
@@ -25,11 +29,14 @@ public class ExerciseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
         todaysDate = LocalDate.now().toString();
+
+        timerTxt = (TextView) findViewById(R.id.timerTxt);
+        numberOfPushupsET = (EditText) findViewById(R.id.NumberOfPushpsTxt);
+
     }
 
     public void saveToDb(View view){
 
-        EditText numberOfPushupsET = (EditText) findViewById(R.id.NumberOfPushpsTxt);
         String numberOfPushups = numberOfPushupsET.getText().toString();
 
         if(Validator.isInteger(numberOfPushups)) {
@@ -39,16 +46,21 @@ public class ExerciseActivity extends AppCompatActivity {
             Log.d("tag", numberOfPushups);
 
             EntryDatabase db = EntryDatabase.getDbInstance(this);
-            Entry entry = new Entry();
-
-            entry.pushups = numberOfPushupsInt;
-            entry.date = todaysDate;
 
             Log.d("tag", "insert started");
 
             // om det inte finns några entries med dagens datum
-            db.entryDao().insertEntry(entry);
+            if(db.entryDao().getSpecificEntry(todaysDate) == null) {
+                Entry entry = new Entry(numberOfPushupsInt, todaysDate);
+                db.entryDao().insertEntry(entry);
+            }
+
             //annars ska entrien med dagens datum uppdateras
+            else{
+
+                db.entryDao().updateSpecificEntry(todaysDate, numberOfPushupsInt);
+
+            }
 
             Log.d("tag", "insert finished");
 
@@ -61,10 +73,19 @@ public class ExerciseActivity extends AppCompatActivity {
 
     public void testShow(View view){
 
-        EntryDatabase db = EntryDatabase.getDbInstance(this);
+        new CountDownTimer(60000, 1000){
 
-        db.entryDao().deleteAllEntires();
+            @Override
+            public void onTick(long ms){
+                timerTxt.setText(String.valueOf("0:"+ms /1000));
+            }
+            @Override
+            public void onFinish(){
+                timerTxt.setText("0:00!!!");
+            }
 
+
+        }.start();
 
     }
 
